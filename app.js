db17820b256186732c338810c682a2236fd79a1e
morris penasso <morrispenasso@outlook.it>
2016-07-22 21:33:08 +0200

    Facebook App loaded

---
+++ app.js
@@ -0,0 +1,36 @@
+﻿var myApp = angular.module("myApp", ["ngRoute", "ngFacebook"]);
+
+myApp.config(["$routeProvider", function ($routeProvider) {
+
+    $routeProvider.when("/home", {
+
+        templateUrl: "views/home.html",
+        controller: "facebookHomeCtrl"
+
+    })
+
+    .otherwise({ redirectTo: "/home" });
+}])
+
+    //configure ngFacebook module
+.config(["$facebookProvider", function ($facebookProvider) {
+
+    //Configure ngFacebook with $facebookProvider object
+    //all sdk functions that use with ngFacebook, will be executed into Facebook application
+    $facebookProvider.setAppId("115647552202807");
+
+    //all sdk function that use with ngFacebook, have this permissions
+    $facebookProvider.setPermissions("email, public_profile, user_posts, publish_actions, user_photos");
+
+}])
+//my Angular Module will load Facebook SDK
+.run(function ($rootScope) {
+
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) { return; }
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+});
---
+++ scripts/facebookHomeCtrl.js
@@ -0,0 +1,66 @@
+﻿myApp.controller("facebookHomeCtrl", function ($scope,$facebook) {
+
+    loadData();
+
+    $scope.ifIsLogged = false;
+
+    $scope.login = function () {
+
+        //login into my Facebook Profile
+        $facebook.login().then(function()   {
+            $scope.ifIsLogged = true;
+            loadData();
+            
+        })
+    };
+
+    $scope.logout = function () {
+
+        $facebook.logout().then(function () {
+            $scope.ifIsLogged = false;
+            loadData();
+        });
+    };
+
+    //function that load my data into my Facebook Profile
+    function loadData() {
+        
+        //Ajax call into my Facebook Profile and i require this informations
+        $facebook.api("/me?fields=first_name,last_name,email,gender,locale,link").then(function (response) {
+
+            $scope.welcomeMessage = "Welcome: " + response.first_name + " " + response.last_name;
+            $scope.infoProfile = response;
+
+            //Require Photo path profile into my Facebook Profile
+            $facebook.api("/me/picture").then(function (response) {
+                $scope.picture = response.data.url;
+
+                //require my posts into my Facebook Profile
+                $facebook.api("/me/posts").then(function (response) {
+
+                    $scope.posts = response.data;
+
+                })
+            });
+        },
+        //if promise of my Ajax call will be negative, show this message into the view
+         function (err) {
+
+             $scope.welcomeMessage = "Please Log in";
+         });
+    };
+
+    $scope.toPost = function () {
+
+        var post = $scope.myPost;
+
+        //request all posts of my Facebook Profile
+        $facebook.api("/me/feed", "post", { message: post }).then(function(response)   {
+        
+            loadData();
+        
+        })
+
+    };
+
+});
